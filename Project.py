ddac90d015962bdc91c13decc8f479f8b92bcae6
Ryan Bellarmin <bellarmin7259@paramusschools.org>
2024-10-24 10:01:24 -0400

    project changes

--- Project.py
+++ Project.py
@@ -5,35 +5,10 @@ wn = turtle.Screen(). bgcolor("white")
 turt = turtle.Turtle()
 turtsec = turtle.Turtle()
 
-ColorRandom = random.randint(1, 2)
-distance = 10
-RandCalc = random.randint(1, 2)
-if RandCalc == 1:
-    for _ in range(50):
-       turtsec.forward()
-    for _ in range(50):
-        if ColorRandom == 1:
-            wn = turtle.Screen(). bgcolor("red")
-            ColorRandom = 2
-        else:
-           if ColorRandom == 2:
-              wn = turtle.Screen(). bgcolor("blue")
-              ColorRandom = 1
-        turt.forward(distance)
-        turt.right(90)
-        distance += 10
-else:
-    if RandCalc == 2:
-     for _ in range(25):
-        if ColorRandom == 1:
-            wn = turtle.Screen(). bgcolor("yellow")
-            ColorRandom = 2
-        else:
-           if ColorRandom == 2:
-              wn = turtle.Screen(). bgcolor("green")
-              ColorRandom = 1
-        turt.forward(distance)
-        turt.right(45)
-        distance += 5
+x = input("What Shape do you want?(Square, Triangle, Circle, or Hexagon)")
+if x == "Square":
+    for _ in range(2):
+        turtsec.forward(50)
+        turtsec.left(90)
 
 wn.exitonclick()
